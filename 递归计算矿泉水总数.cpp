e353db6975a8a3cf8f7c324aa3aba3544a5b7fcb
kxk20001995 <60969375+kxk20001995@users.noreply.github.com>
2020-03-11 10:53:09 +0800

    递归计算矿泉水总数题目
    递归计算矿泉水总数题目

---
+++ 递归计算矿泉水总数.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int kkk(int a, int b)
+{
+	if(a<2&&b<3) return 0;
+	else	return a/2+b/3+kkk(a%2+a/2+b/3, b%3+a/2+b/3);
+}
+int main()
+{
+	int x;
+	cin>>x;
+	cout<<x+kkk(x, x);
+	return 0;
+}
+
+
