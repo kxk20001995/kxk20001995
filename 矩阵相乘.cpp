44e26f5478beb117b5117538144352fc915df64f
kxk20001995 <60969375+kxk20001995@users.noreply.github.com>
2020-03-11 10:46:15 +0800

    矩阵运算题目
    矩阵运算题目

---
+++ 矩阵相乘.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[3][3], b[3][3], c[3][3], i, j;
+	for(i=0; i<3; i++)	for(j=0; j<3; j++) cin>>a[i][j];
+	for(i=0; i<3; i++)	for(j=0; j<3; j++) cin>>b[i][j];
+	for(i=0; i<3; i++)	for(j=0; j<3; j++) c[i][j]=a[i][0]*b[0][j]+a[i][1]*b[1][j]+a[i][2]*b[2][j];
+	for(i=0; i<3; i++)	
+	{
+		for(j=0; j<3; j++) cout<<c[i][j]<<" ";
+		cout<<"\n";
+	}
+	return 0;
+}
+
+
+
