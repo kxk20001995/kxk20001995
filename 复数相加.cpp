7f096b6d1787c04ad5b47bfd9f84b1cf037d98e5
kxk20001995 <60969375+kxk20001995@users.noreply.github.com>
2020-03-11 10:52:25 +0800

    复数相加题目
    复数相加题目

---
+++ 复数相加.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	double a, b, c, d;
+	cin>>a>>b>>c>>d;
+	cout<<setiosflags(ios::fixed)<<setprecision(2)<<a-c<<' '<<b-d;
+	return 0;
+}
+
+
