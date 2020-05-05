441e024d97c257ec8613457274794129be9f99ba
kxk20001995 <60969375+kxk20001995@users.noreply.github.com>
2020-05-05 10:57:09 +0800

    申请空间时，判断电脑内存是否足够
    申请空间时，判断电脑内存是否足够

--- 源.cpp
+++ 源.cpp
@@ -1,61 +1,59 @@
-/*编程序输出魔方矩阵。魔方矩阵是一个n×n奇数矩阵，其中n2个元素分别是1,2,3,…, n2，并且任意一行、任意一列以及主副对角线的所有数之和均相等，且都等于n(n2+1)/2。
-[提示]魔方矩阵数字排列规则如下：
-1.	数字1在矩阵的第一行中间一列；
-2.	从数字2到n2的各数依次存放在比前一个数行数减一、列数加一的位置（即前一个数的右上角位置）；
-3.	若上一个数在第一行，则下一个数在做后一行；
-4.	若上一个数在最后一列，下一个数在第一列；
-5.	如果按上述规则寻找的位置上已经放置有数，则将数字放在上一个数的下面。
-三阶魔方矩阵如下图所示：
-8	1	6
-3	5	7
-4	9	2
-*/ 
-
+/*C++编写一个函数模拟动态分配数组的内存空间。
+函数具有一个整型参数，它代表待分配的一个整型数组元素的个数。 
+函数应当完成必要的出错检测（如参数为0或负数），
+如果内存空间充足，那么就分配需要的空间，并返回指向该空间的 指针；否则返回一个空指针*/
 #include<iostream>
-#include<vector>
+#include<cstdlib>
+#include<exception>
 using namespace std;
+
+int* p;		//全局变量
+
+int* ARRAY(int x)
+{
+	if (x <= 0)		//出错检测
+	{
+		cout << "false!!!" << endl;
+		exit(1);		//表示非正常运行导致退出程序
+	}
+
+	//try		
+	//{
+	//	p = new int[x];		//如果内存不够，返回NULL指针
+	//}
+	//catch (exception)
+	//{
+	//	int* p0 = NULL;
+	//	cout << "指针申请失败，内存不足！！！" << endl;
+	//	return p0;
+	//}
+	//cout << p << endl;
+	//return p;
+
+	//方法二：（内存不够时自动中止）
+	if (p == NULL)
+	{
+		cout << "指针申请失败，内存不足！！！" << endl;
+		cout << p << endl;
+		delete[] p;		//释放p指针
+
+		int* p0 = NULL;
+		return p0;		//返回一个空指针
+	}
+	else
+	{
+		cout << p << endl;
+		return p;
+	}
+
+	//你也可以使用vector或者malloc函数
+}
 
 int main()
 {
-	int n, row, column, temp_row, temp_column;
-	cin >> n;
-	vector<vector<int> >a(n);	//动态定义n行的二维数组
-	for (int i = 0; i < n; i++)	//初始化数组
-	{
-		for (int j = 0; j < n; j++) a[i].push_back(0);
-	}
-
-	a[0][n / 2] = 1;
-	row = 0;
-	column = n / 2;	//记录元素的行(row)和列(column)
-	for (int num = 2; num <= n * n; num++)
-	{
-		temp_row = row;
-		temp_column = column;		//temp_row记录上一个数的行，temp_column记录上一个数的列
-
-		if (row == 0) row = n - 1;	//若上一个数在第一行，则下一个数在做后一行
-		else row -= 1;	//比前一个数行数减一
-
-		if (column == n - 1) column = 0;	//若上一个数在最后一列，下一个数在第一列
-		else column += 1;	//比前一个数列数加一
-
-		if (a[row][column] != 0)	//如果按规则寻找的位置上已经放置有数，则将数字放在上一个数的下面
-		{
-			if (temp_row == n - 1) row = 0;	//如果row在最后一行，则下一个数在第一行
-			else row = temp_row + 1;	//否则将数字放在上一个数的下面
-
-			column = temp_column;	//将这个数还原到与上一个数的列数相同
-
-			a[row][column] = num;
-		}
-		else a[row][column] = num;
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++) cout << a[i][j] << ' ';
-		cout << endl;
-	}
-
+	int x;
+	cin >> x;		//确定ARRAY（）函数实参值
+	ARRAY(x);
+	delete[]p;		//释放p指针
 	return 0;
 }
